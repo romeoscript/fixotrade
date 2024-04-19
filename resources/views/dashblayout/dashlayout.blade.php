771014b4cd1d8d1070e13e8adb81ae167572e47a
Romanus Chukwuemeka <romeobourne211@gmail.com>
2024-04-19 05:59:07 +0100

    Update dashlayout.blade.php

--- resources/views/dashblayout/dashlayout.blade.php
+++ resources/views/dashblayout/dashlayout.blade.php
@@ -836,8 +836,18 @@
         const dateElement = document.getElementById("date");
         dateElement.innerText = "Copyright ©  " + fullYear;
     </script>
-
-    <script src="//code.tidio.co/2uc3htrmnjtvmjzbggc36csjyu87a1rm.js" async></script>
-</body>
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '08bf8136822f2847e9ea57118118155dc2c1bb84';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
+  </body>
 
 </html>
